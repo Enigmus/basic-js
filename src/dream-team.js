d190524e3327f45d5b2e81e410ef712925641d8c
sd <sd_net@mail.ru>
2023-12-15 18:06:29 +0300

    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,49 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+    return Array.isArray(members)
+        ? members
+              .filter((m) => typeof m === "string")
+              .map((m) => m.trim())
+              .sort((a, b) => a.localeCompare(b))
+              .map((m) => m[0].toUpperCase())
+              .reduce((acc, el) => (acc += el), "")
+        : false;
 }
 
+/* console.log(createDreamTeam(["Matt", "Ann", "Dmitry", "Max"])); //'ADMM'
+console.log(createDreamTeam(["Olivia", 1111, "Lily", "Oscar", true, null])); // 'LOO'
+
+console.log(
+    createDreamTeam([
+        ["David Abram"],
+        ["Robin Attfield"],
+        "Thomas Berry",
+        ["Paul R.Ehrlich"],
+        "donna Haraway",
+        " BrIaN_gOodWiN  ",
+        {
+            0: "Serenella Iovino",
+        },
+        "Erazim Kohak",
+        "  val_plumwood",
+    ])
+    
+);//"BDETV" */
+
 module.exports = {
-  createDreamTeam
+    createDreamTeam,
 };
