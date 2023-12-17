49d08e1d3de42c60fa33c7ac7112d80dedc5cf30
sd <sd_net@mail.ru>
2023-12-17 22:10:40 +0300

    feat: add solution

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+    // итоговая матрица
+    let field = [];
+    // количество строк
+    let colRows = matrix.length;
+    // количество столбцов
+    let colColumns = matrix[0].length;
+    // заполняем ее 0, в размер с исходной
+    for (let i = 0; i < matrix.length; i++) {
+        let row = [];
+        for (let j = 0; j < matrix[0].length; j++) {
+            row.push(0);
+        }
+        field.push(row);
+        row = [];
+    }
+    // если в матрице находим true, соседние ячейки увеличиваем на 1
+    for (let i = 0; i < colRows; i++) {
+        for (let j = 0; j < colColumns; j++) {
+            if (matrix[i][j] === true) {
+                let rowPrev = i - 1 < 0 ? 0 : i - 1;
+                let rowNext = i + 1 > colRows ? i : i + 1;
+                for (let row = rowPrev; row <= rowNext; row++) {
+                    let columnPrev = j - 1 < 0 ? 0 : j - 1;
+                    let columnNext = j + 1 > colColumns ? j : j + 1;
+                    for (let col = columnPrev; col <= columnNext; col++) {
+                        if (row === i && col === j) continue;
+                        field[row][col] += 1;
+                    }
+                }
+            }
+        }
+    }
+    return field;
 }
 
+/* console.log(
+    minesweeper([
+        [true, false, false],
+        [false, true, false],
+        [false, false, false],
+    ])
+); */
+
 module.exports = {
-  minesweeper
+    minesweeper,
 };
