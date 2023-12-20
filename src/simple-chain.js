3d9b1cf42e341f461214420b71d93f3ffc392fec
sd <sd_net@mail.ru>
2023-12-20 23:23:12 +0300

    feat: add solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,27 +5,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  chain : [],
+  getLength() {    
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if(typeof position !== 'number' || position <  1 || position > this.getLength()   || !Number.isInteger(position)){      
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!");
+    }else{
+    this.chain.splice(position-1,1);
+    return this;
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let out = ''; 
+    for(let i = 0; i < this.getLength(); i++){
+      out += `( ${this.chain[i]} )~~`
+    }
+    out = out.substring(0,out.length - 2)
+    this.chain = [];
+    return out
   }
 };
+
+console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain()); 
+//'( null )~~( GHI )~~( 333 )~~( 0 )~~( GHI )';
+
+console.log(chainMaker.addLink(function () { }).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain())
+//, '( 3rd )~~( function () { } )
+
 
 module.exports = {
   chainMaker
